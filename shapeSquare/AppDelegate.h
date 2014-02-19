94f9160b42133772279182c94430ff7131726608
Lavi Avigdor 2 <laviavigdor@Shesek.local>
2014-02-19 20:00:32 +0200

    Initial Commit

---
+++ shapeSquare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shapeSquare
+//
+//  Created by Lavi Avigdor 2 on 2/19/14.
+//  Copyright (c) 2014 Twtrland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shapeSquare/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  shapeSquare
+//
+//  Created by Lavi Avigdor 2 on 2/19/14.
+//  Copyright (c) 2014 Twtrland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
